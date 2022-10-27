aa2f6b00d73f6c8e7dfb2333d62f5fc9a9097b1e
nebulosa2007 <nebulosa2007@yandex.ru>
2022-10-27 15:32:36 +0300

    added 07

---
+++ euler07.py
@@ -0,0 +1,25 @@
+# Какое число является 10001-м простым числом?
+
+from itertools import islice, count
+from datetime import datetime
+
+n = 10_001
+
+
+def primes():
+    ints = count(2)
+    while True:
+        p = next(ints)
+        yield p
+        ints = filter(p.__rmod__, ints)
+
+
+# 1 Вариант
+start_time = datetime.now()
+print(list(islice(primes(), n))[-1])
+print(datetime.now() - start_time)
+
+# 2 Вариант
+start_time = datetime.now()
+pass
+print(datetime.now() - start_time)
