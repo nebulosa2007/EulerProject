8948fc9da89040856b1bd27438757a98f7a669bf
nebulosa2007 <nebulosa2007@yandex.ru>
2022-11-08 18:44:16 +0300

    refactoring 22

--- euler22.py
+++ euler22.py
@@ -5,23 +5,28 @@
 
 from datetime import datetime
 from requests import get
+from euler11 import readmatrix
 
 n = "https://projecteuler.net/project/resources/p022_names.txt"
+
+
+def names_points(*names):
+    return sum(sum(ord(j) - 64
+                   for j in list(names[i])) * (i + 1)
+               for i in range(len(names)))
 
 
 if __name__ == '__main__':
     # 1 вариант
     start_time = datetime.now()
-    response = get(n, timeout=3)
-    if response.status_code != 200:
-        exit()
-    names = sorted(response.content.decode().replace('"', '').split(','))
-    print(sum(sum(ord(j) - 64
-                  for j in list(names[i])) * (i + 1)
-              for i in range(len(names))))
+    print(names_points(*sorted(readmatrix("euler22.txt", mode='text'))))
     print(datetime.now() - start_time)
 
     # 2 вариант
     start_time = datetime.now()
-    pass
+    response = get(n, timeout=3)
+    if response.status_code != 200:
+        exit()
+    names = sorted(response.content.decode().replace('"', '').split(','))
+    print(names_points(*names))
     print(datetime.now() - start_time)
