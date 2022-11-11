b9b437a4c1596d3f8cf63bad83332b7ce02c1aa0
nebulosa2007 <nebulosa2007@yandex.ru>
2022-11-11 16:58:03 +0300

    clean 23

--- euler23.py
+++ euler23.py
@@ -9,18 +9,13 @@ n = 28_123
 start_time = datetime.now()
 number_list = [0 for _ in range(n)]
 start_sum = sum([_ for _ in range(n)])
-# print(start_sum)
 for i in range(n):
     divisor_list = factors_long_time(i)[:-1]
-    # print(i, sum(divisor_list))
     if sum(divisor_list) > i:
         number_list[i] = i
-        # print(i, sum(divisor_list))
-# print(number_list)
 temp = [i * 2 for i in set(number_list) if i * 2 < n]
 print(temp)
 print(start_sum - sum(i * 2 for i in set(number_list) if i * 2 < n))
-# print(sum(number_list))
 print(datetime.now() - start_time)
 
 # 2 вариант
