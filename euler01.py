d9e1abaa204aa39e224396c33500cfd5afa0be89
nebulosa <nebulosa2007@yandex.ru>
2022-12-14 20:32:11 +0300

    first step of huge refactoring

--- euler01.py
+++ euler01.py
@@ -1,21 +1,22 @@
 # Найдите сумму всех чисел меньше 1000, кратных 3 или 5.
 
-from project_euler_defs import *
+from datetime import datetime as dt
+import functools as ft
 
 n = 1_000
 
 # Вариант 1
-start_time = datetime.now()
+start_time = dt.now()
 sum = 0
 for i in range(n):
     if i % 3 == 0 or i % 5 == 0: sum += i
 else:
     print(sum)
-print(datetime.now() - start_time)
+print(dt.now() - start_time)
 
 # Вариант 2
-start_time = datetime.now()
-print(reduce(lambda x, y: x + y,
+start_time = dt.now()
+print(ft.reduce(lambda x, y: x + y,
              list(filter(lambda x: x % 3 == 0 or x % 5 == 0,
                          list(range(n))))))
-print(datetime.now() - start_time)
+print(dt.now() - start_time)
