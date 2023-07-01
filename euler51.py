a0d3a40ae874cd5b267e50d994770e1b690accb1
nebulosa <nebulosa2007@yandex.ru>
2023-07-01 17:58:25 +0300

    Added 51

---
+++ euler51.py
@@ -0,0 +1,14 @@
+# Найдите наименьшее простое число, которое является одним из восьми простых чисел, 
+# полученных заменой части цифр (не обязательно соседних) одинаковыми цифрами.
+
+from project_euler_defs import *
+
+# 1 вариант
+start_time = datetime.now()
+pass
+print(datetime.now() - start_time)
+
+# 2 вариант
+start_time = datetime.now()
+pass
+print(datetime.now() - start_time)
